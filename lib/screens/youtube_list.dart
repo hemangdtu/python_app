69c3e82e7bff2bca04013dc3467be5b48e02fca4
hemangdtu <hemang.dtu@gmail.com>
2021-05-26 01:25:41 +0530

    YouTube API integration successful

--- lib/screens/youtube_list.dart
+++ lib/screens/youtube_list.dart
@@ -1,41 +1,219 @@
 import 'package:flutter/material.dart';
-import 'package:python_app/meta/data.dart';
-import 'package:python_app/meta/hyperlinks.dart';
-import 'package:python_app/models/widgetDirectory.dart';
+import 'package:python_app/models/channel_model.dart';
+import 'package:python_app/models/video_model.dart';
+import 'package:python_app/screens/video_screen.dart';
+import 'package:python_app/services/youtube_api.dart';
 
-// TODO : Updated YouTube Channel Linking and edit class accordingly.
+// TODO : UI enhancements
 
-class YouTubePage extends StatelessWidget {
+class YouTubePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return youtubeList(context);
-  }
+  _YouTubePageState createState() => _YouTubePageState();
 }
 
-Widget youtubeList(BuildContext context) {
-  List<Widget> videoList = [];
-  for (int i = 0; i < videoTileStrings.length; i++) {
-    videoList.add(
-      VideoListElement(
-        title: videoTileStrings[i],
-        link: VideoLinks.linkMap[videoTileStrings[i]],
+class _YouTubePageState extends State<YouTubePage> {
+  Channel _channel;
+
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initChannel();
+  }
+
+  _initChannel() async {
+    Channel channel = await APIService.instance
+        .fetchChannel(channelId: 'UCwne7qrrSGVbhq07hBelKig');
+    setState(() {
+      _channel = channel;
+    });
+  }
+
+  _buildProfileInfo() {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(20.0),
+      height: 100.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0, 1),
+            blurRadius: 6.0,
+          ),
+        ],
+      ),
+      child: Row(
+        children: <Widget>[
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 35.0,
+            backgroundImage: NetworkImage(_channel.profilePictureUrl),
+          ),
+          SizedBox(width: 12.0),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  _channel.title,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  '${_channel.subscriberCount} subscribers',
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
-  return Container(
-    decoration: BoxDecoration(
-      gradient: LinearGradient(
-        colors: [
-          Color(0xFF7E9BE0),
-          Color(0xFF2E8BC0),
-        ],
-        begin: Alignment.bottomCenter,
-        end: Alignment.topCenter,
-        tileMode: TileMode.clamp,
+
+  _buildVideo(Video video) {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => VideoScreen(id: video.id),
+        ),
       ),
-    ),
-    child: ListView(
-      children: videoList,
-    ),
-  );
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+        padding: EdgeInsets.all(10.0),
+        height: 140.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 1),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Row(
+          children: <Widget>[
+            Image(
+              width: 150.0,
+              image: NetworkImage(video.thumbnailUrl),
+            ),
+            SizedBox(width: 10.0),
+            Expanded(
+              child: Text(
+                video.title,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _loadMoreVideos() async {
+    _isLoading = true;
+    List<Video> moreVideos = await APIService.instance
+        .fetchVideosFromPlaylist(playlistId: _channel.uploadPlaylistId);
+    List<Video> allVideos = _channel.videos..addAll(moreVideos);
+    setState(() {
+      _channel.videos = allVideos;
+    });
+    _isLoading = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF7E9BE0),
+            Color(0xFF2E8BC0),
+          ],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+          tileMode: TileMode.clamp,
+        ),
+      ),
+      child: _channel != null
+          ? NotificationListener<ScrollNotification>(
+              onNotification: (ScrollNotification scrollDetails) {
+                if (!_isLoading &&
+                    _channel.videos.length != int.parse(_channel.videoCount) &&
+                    scrollDetails.metrics.pixels ==
+                        scrollDetails.metrics.maxScrollExtent) {
+                  _loadMoreVideos();
+                }
+                return false;
+              },
+              child: ListView.builder(
+                itemCount: 1 + _channel.videos.length,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) {
+                    return _buildProfileInfo();
+                  }
+                  Video video = _channel.videos[index - 1];
+                  return _buildVideo(video);
+                },
+              ),
+            )
+          : Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(
+                  Theme.of(context).primaryColor, // Red
+                ),
+              ),
+            ),
+    );
+  }
 }
+
+// class YouTubePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return youtubeList(context);
+//   }
+// }
+
+// Widget youtubeList(BuildContext context) {
+//   return Container(
+//     decoration: BoxDecoration(
+//       gradient: LinearGradient(
+//         colors: [
+//           Color(0xFF7E9BE0),
+//           Color(0xFF2E8BC0),
+//         ],
+//         begin: Alignment.bottomCenter,
+//         end: Alignment.topCenter,
+//         tileMode: TileMode.clamp,
+//       ),
+//     ),
+//     child: ListView(
+//       children: [
+//         for (int i = 0; i < videoTileStrings.length; i++)
+//           VideoListElement(
+//             title: videoTileStrings[i],
+//             link: VideoLinks.linkMap[videoTileStrings[i]],
+//           )
+//       ],
+//     ),
+//   );
+// }
