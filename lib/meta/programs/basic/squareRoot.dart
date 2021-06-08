39749fb029024f0b75e28f606866dc4b6f3101d5
hemangdtu <hemang.dtu@gmail.com>
2021-06-08 21:38:07 +0530

    Updating examples

---
+++ lib/meta/programs/basic/squareRoot.dart
@@ -0,0 +1,29 @@
+String squareRootCode = '''
+### Square Root
+ 
+#### Program
+```
+# Python Program to calculate the square root
+
+num = float(input('Enter a number: '))
+
+numSqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,numSqrt))
+
+```
+#### Input
+```
+16
+
+```
+#### Output
+```
+Enter a number: 16
+The square root of 16.000 is 4.000
+
+```
+''';
+
+String squareRootExplain = '''
+In this program, we store the number in num and find the square root using the ** exponent operator. This program works for all positive real numbers.
+''';
